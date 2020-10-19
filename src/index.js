f045bf01ddd180b29b0bc2cb7e1b9d8d20c0a434
Vitali_Vianhura <vengura.code@gmail.com>
2020-10-19 23:43:51 +0300

    feat: implement `towerSort` function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+      let list;
+      if (i % 2 !== 0) {
+          list = matrix[i].reverse();
+      } else {
+          list = matrix[i];
+      }
+      for (let j = 0; j < list.length; j++) {
+          result.push(list[j]);
+      }
+  }
+  return result;
 }
